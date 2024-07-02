439cae5022c77d81c07be596a9a7100465ab5f07
Shifat-project <shifat32153@gmail.com>
2024-07-02 14:40:37 +0600

    first commit

---
+++ easy_practice.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "1-1" << endl;
+    cout << "2-4" << endl;
+    cout << "3-9" << endl;
+    cout << "4-16" << endl;
+    cout << "5-25" << endl;
+
+    return 0;
+}
